0f11eb009107f1516981fb340f4a7f29d146bf6f
Nhat <nhat@users.noreply.github.com>
2016-09-26 12:36:10 +0200

    disable history sharing between sessions

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -29,10 +29,10 @@ HISTSIZE=10000
 SAVEHIST=10000
 
 setopt NO_HIST_VERIFY # runs last command after enter
-setopt SHARE_HISTORY # share history between sessions
+unsetopt SHARE_HISTORY # don't share history between sessions
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt INC_APPEND_HISTORY  # adds history incrementally
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
