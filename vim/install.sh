ccb64267599dd89fdee23c2abd71585f28047a8f
Nhat <nhat@users.noreply.github.com>
2016-11-21 01:02:49 +0100

    clean vi config

--- vim/install.sh
+++ vim/install.sh
@@ -6,5 +6,5 @@
 # using Vim.
 
 # Install Vim packages manager
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
