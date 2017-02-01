94ac3d46772855f888824413609e2e5bed0fc038
Nhat <nhat@users.noreply.github.com>
2017-02-01 14:20:16 +0100

    add mid-word completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,5 +1,7 @@
 # matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' completer _complete
+zstyle ':completion:*' matcher-list '' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' '+l:|=* r:|=*'
+
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
