908150fdfbba253b3a03f0490a6dd056e07c7710
Nhat <nhat@users.noreply.github.com>
2018-09-21 14:43:25 +0200

    add parameter support for curl

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -21,12 +21,31 @@ alias s='ag'
 alias c='fasd_cd -d'
 alias f='fd --follow --hidden'
 alias xml='(if [[ -t 1 ]] ; then xmllint --format - | source-highlight -f esc -s xml --style-file xml.style --data-dir=/usr/local/share/source-highlight ; else xmllint --format -; fi)'
-alias curl='curl -sS -D /dev/stderr'
 alias update_zsh_plugins='antibody bundle < $DOTFILES_ROOT/zsh/plugins >! ~/.zsh/.zsh_plugins && antibody update'
 
 function take() {
   mkdir -p $1
   cd $1
+}
+
+# Show short response status for each requests
+function curl() {
+    silent=0
+    # silent option
+    for var in "$@"; do
+        if [[ $var == "-"* ]] && [[ $var != "--"* ]]; then
+            if [[ $var =~ "s" ]] ||
+               [[ $var =~ "v" ]]; then
+                silent=1
+            fi
+        fi
+    done
+
+    if [[ $silent == 0 ]]; then
+        /usr/local/bin/curl -sS -D /dev/stderr $@
+    else
+        /usr/local/bin/curl $@
+    fi
 }
 
 # Use nvim if installed
