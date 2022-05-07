c623fcb6084fc2bb31bbb941955af24234442f64
Nhat <nhat@users.noreply.github.com>
2022-05-07 10:31:22 +0200

    update path

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$DOTFILES_ROOT/bin:/usr/local/bin:/usr/local/sbin:.:/opt/homebrew/opt/gnu-sed/libexec/gnubin:$HOME/Developer/bin:${KREW_ROOT:-$HOME/.krew}/bin:$PATH/"
+export PATH="$DOTFILES_ROOT/bin:/usr/local/bin:/usr/local/sbin:.:/opt/homebrew/opt/gnu-sed/libexec/gnubin:$HOME/Developer/bin:${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
