5c4f24c24b42510878c86bca82f8d0b0dba6c479
Nhat <nhat@users.noreply.github.com>
2022-08-15 16:47:19 +0200

    update jq and source-highlight

--- config/source-highlight/install.sh
+++ config/source-highlight/install.sh
@@ -1,2 +1,2 @@
-ln -sf $DOTFILES_ROOT/config/source-highlight/json.style /usr/local/share/source-highlight/json.style
-ln -sf $DOTFILES_ROOT/config/source-highlight/xml.style /usr/local/share/source-highlight/xml.style
+ln -sf $DOTFILES_ROOT/config/source-highlight/json.style /opt/homebrew/share/source-highlight/json.style
+ln -sf $DOTFILES_ROOT/config/source-highlight/xml.style /opt/homebrew/share/source-highlight/xml.style
