387017a4581be95622990530b309a2e10a09fc39
Nhat <nhat@users.noreply.github.com>
2022-10-30 18:08:02 +0100

    Update homebrew non M1 path

--- system/grc.zsh
+++ system/grc.zsh
@@ -1,5 +1,8 @@
 # grc colorizes nifty unix tools all over the place
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
-  source /opt/homebrew/etc/grc.zsh
+if (( $+commands[grc] )) && (( $+commands[brew] )) then
+  if [[ $(arch) == 'arm64' ]]; then
+    source /opt/homebrew/etc/grc.zsh
+  else
+    source /usr/local/etc/grc.zsh
+  fi
 fi
