5abd2dcbf317ab923ce6b90c1a4cf9ca7c08dab9
Nhat <nhat@users.noreply.github.com>
2025-06-11 11:51:11 +0200

    Update homebrew packages

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,10 +1,10 @@
-#!/bin/zsh
+# !/bin/zsh
 #
 # homebrew
 
 echo "🍺 Setting up Homebrew"
 # check for Homebrew
-if test ! $(which brew); then
+if ! command -v brew >/dev/null 2>&1; then
   echo "Installing Homebrew for you"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
@@ -65,14 +65,20 @@ done
 # install apps from App Store
 echo "Installing apps from App Store..."
 apps=(
+  497799835 # Xcode
+  6471380298 # StopTheMadness Pro
   1480933944 # Vimari
   1365531024 # 1Blocker
-  1445328303 # prettyJSON
+  1458969831 # JSONPeep
+  1606897889 # Consent-O-Matic
   1118136179 # AutoMute
   688211836  # EasyRes
-  1543920362 # Displaperture
-  1568262835 # Super Agent
-  1376402589 # StopTheMadness
+  1586435171 # Actions
+  1287239339 # ColorSlurp
+  6443796196 # FreeScaler
+  1510150275 # Lenscape
+  1289583905 # Pixelmator Pro
+  1550457805 # Soro
 )
 for app in $apps; do
   mas install $app
