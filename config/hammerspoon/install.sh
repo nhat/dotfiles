170245f6bd9c971c3433c6219d120b432925fd78
Nhat <nhat@users.noreply.github.com>
2025-06-15 11:48:10 +0200

    Install hammerspoon extensions

--- config/hammerspoon/install.sh
+++ config/hammerspoon/install.sh
@@ -1,6 +1,43 @@
-mkdir -p $HOME/.config/hammerspoon
-ln -sf $DOTFILES_ROOT/config/hammerspoon/init.lua $HOME/.config/hammerspoon/init.lua
+#!/usr/bin/env zsh
 
-# make hammerspoon use the config file in ~/.config/hammerspoon
-defaults write org.hammerspoon.Hammerspoon MJConfigFile "~/.config/hammerspoon/init.lua"
+# Define base path
+BASE_PATH="$HOME/.config/hammerspoon"
 
+# Install Hammerspoon configuration
+echo "Installing Hammerspoon configuration..."
+mkdir -p "$BASE_PATH"
+ln -sf "$DOTFILES_ROOT/config/hammerspoon/init.lua" "$BASE_PATH/init.lua"
+
+# Make Hammerspoon use the config file in ~/.config/hammerspoon
+echo "Setting Hammerspoon config file path to $BASE_PATH/"
+defaults write org.hammerspoon.Hammerspoon MJConfigFile "$BASE_PATH/init.lua"
+
+# Clone the Spoons repository
+echo "\nCloning Spoons repository..."
+mkdir -p "$BASE_PATH/Spoons"
+cd "$BASE_PATH/Spoons"
+
+BASE_URL="https://github.com/nhat/"
+spoons=(
+  "Marginator.spoon"
+  "MoveWindow.spoon"
+  "MoveSpace.spoon"
+)
+
+for spoon in "${spoons[@]}"; do
+  TARGET_DIR="${spoon}"
+  if [ -d "$TARGET_DIR" ]; then
+    echo "Skipping $spoon: Directory already exists."
+    continue
+  fi
+
+  REPO_URL="${BASE_URL}${spoon}.git"
+
+  echo "Cloning $spoon..."
+  if git clone "$REPO_URL" "$TARGET_DIR"; then
+    echo "$spoon cloned successfully."
+  else
+    echo "Failed to clone $spoon." >&2
+    exit 1
+  fi
+done
