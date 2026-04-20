21ce20d6bdcffb857907d7ac35439890be37ae43
Nhat <nhat@users.noreply.github.com>
2026-04-20 13:43:27 +0200

    Use env vars for gh host config to avoid exposing company info

--- system/gh.zsh
+++ system/gh.zsh
@@ -1,8 +1,9 @@
 # Auto-switch gh host based on current directory.
 # Uses GH_HOST env var which gh respects for all commands.
+# Set WORK_GH_HOST and WORK_GH_DIR in ~/.localrc to configure.
 _gh_host_chpwd() {
-  if [[ "$PWD" == "$HOME/Developer/eBay/src"* ]]; then
-    export GH_HOST=github.corp.ebay.com
+  if [[ -n "$WORK_GH_HOST" && -n "$WORK_GH_DIR" && "$PWD" == "$WORK_GH_DIR"* ]]; then
+    export GH_HOST=$WORK_GH_HOST
   else
     unset GH_HOST
   fi
