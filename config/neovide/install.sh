5d8fe21f085c918975b0df8680918cb7eead6dac
Nhat <nhat@users.noreply.github.com>
2026-04-30 22:04:43 +0200

    neovide: add custom icon

--- config/neovide/install.sh
+++ config/neovide/install.sh
@@ -2,3 +2,4 @@
 
 mkdir -p "$HOME/.config/neovide"
 ln -sf "$DOTFILES_ROOT/config/neovide/config.toml" "$HOME/.config/neovide/config.toml"
+ln -sf "$DOTFILES_ROOT/config/neovide/icon.icns" "$HOME/.config/neovide/icon.icns"
